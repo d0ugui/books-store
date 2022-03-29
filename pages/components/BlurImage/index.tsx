9fd56d81266b3bfb007abdb376c80c411ca80f4d
Douglas Oliveira <douglaspo_97@outlook.com>
2022-03-28 22:36:42 -0300

    feat: blur-up image placeholders

--- pages/components/BlurImage/index.tsx
+++ pages/components/BlurImage/index.tsx
@@ -1,19 +1,30 @@
 import Image from "next/image";
+import { useState } from "react";
 
-export default function BlurImage() {
+import cn from "../../../src/utils/combineClass";
+
+export function BlurImage() {
+  const [isLoading, setLoading] = useState(true);
+
   return (
     <a href="#" className="group">
-      <div className="aspect-w-1 aspect-h-1 xl:aspect-w-7 xl:aspect-h-8 w-full overflow-hidden rounded-lg bg-gray-200">
+      <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
         <Image
           alt=""
           src="https://bit.ly/placeholder-img"
           layout="fill"
           objectFit="cover"
-          className="group-hover:opacity-75"
+          className={cn(
+            'group-hover:opacity-75 duration-700 ease-in-out',
+            isLoading
+              ? 'grayscale blur-2xl scale-110'
+              : 'grayscale-0 blur-0 scale-100'
+          )}
+          onLoadingComplete={() => setLoading(false)}
         />
       </div>
-      <h3 className="mt-4 text-sm text-gray-700">Douglas Oliveira</h3>
-      <p className="mt-1 text-lg font-medium text-gray-900">@d0ugui</p>
+      <h3 className="mt-4 text-sm text-gray-700">Lee Robinson</h3>
+      <p className="mt-1 text-lg font-medium text-gray-900">@leeerob</p>
     </a>
-  )
+  );
 }
